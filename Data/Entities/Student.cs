8b9c88a98c929e4e3730dda7ed81a4c16987ebd3
Ludwin Bladimir Portillo Chevez <U20180723@univo.edu.sv>
2021-02-04 11:44:57 -0600

    Cambios finales

---
+++ Data/Entities/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace UniversidadDeOriente.Data.Entities
+{
+	public class Student
+	{
+		public int ID { get; set; }
+		[Display(Name = "Apellidos")]
+		public String LastName { get; set; }
+		[Display(Name = "Primer Nombre")]
+		public String FirtsMidName { get; set; }
+		[Display(Name = "Enrollment Date")]
+		public DateTime EnrollmentDate { get; set; }
+		[Display(Name = "Nombre Completo")]
+
+		public String FullName
+		{
+			get
+			{
+				return FirtsMidName + " " + LastName;
+			}
+		}
+		public ICollection<Enrollment> Enrollments { get; set; }
+	}
+}
